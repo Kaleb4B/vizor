a580a4c88e0957a4e163cbff5528b112538e592e
kalebdap <kalebdap@users.noreply.github.com>
2026-08-10 16:24:02 +0700

    fix: update active Cloudflare Tunnel URL to leading-cached-before-biological

--- sdk/clickguard.js
+++ sdk/clickguard.js
@@ -2,7 +2,7 @@
   'use strict';
 
   // Default Cloudflare Tunnel endpoint for live ingestion
-  var DEFAULT_ENDPOINT = 'https://scales-common-consequences-acquired.trycloudflare.com/api/events';
+  var DEFAULT_ENDPOINT = 'https://leading-cached-before-biological.trycloudflare.com/api/events';
 
   var ClickGuard = {
     websiteId: null,
